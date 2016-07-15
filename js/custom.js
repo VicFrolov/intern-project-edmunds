70d6f590bbb52b93146d28010ba861f0444005e7
Frolov <vfrolov@vfrolov-mbp112.local>
2016-07-15 10:13:10 -0700

    pushing changes of js trying to get the slide animation working

--- js/custom.js
+++ js/custom.js
@@ -2,4 +2,16 @@ $(document).ready(function(){
     
     $('.slider').slider({full_width: true});
 
+
+    var $ct = $('#container-search');
+    
+    $("#trigger").click(function(){
+        console.log('trigger works')
+
+        $ct.children(".content:first").animate({ "margin-left": "-800px" }, "slow", function(){
+            console.log("lol")
+            $(this).remove();
+        });
+    });
+
 });
