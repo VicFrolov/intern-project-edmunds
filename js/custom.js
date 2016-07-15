24044619bc9cbefbaa09030bc7286eba54d94d66
Frolov <vfrolov@vfrolov-mbp112.local>
2016-07-15 10:54:23 -0700

    implemented slider

--- js/custom.js
+++ js/custom.js
@@ -2,20 +2,20 @@ $(document).ready(function(){
     
     $('.slider').slider({full_width: true});
 
+    var $gal = $('#gallery'),
+        $sli = $('#slider'),
+        $box = $('div',$sli),
+        W    = $gal.width(), // 500
+        N    = $box.length,  // 3
+        C    = 0;            // a counter
 
-    var $ct = $('#container-search');
-    
-    $("#trigger").click(function(){
-        console.log('trigger works')
+    $sli.width(W*N);
 
-        $('#content1, #content2').animate({width: '0px'}, 1000, function() {
-                    $(this).hide();
-                    $('#content1, #content2').animate({width: '573px'}, 1000, function() {
-                            $(this).show();
-                        }
-                    );
-                }
-            );
-    });
+
+    $('#prev, #next').click(function(){
+      C = (this.id=='next' ? ++C : --C) <0 ? N-1 : C%N;
+      $sli.stop().animate({left: -C*W },900);
+    }); 
+
 
 });
