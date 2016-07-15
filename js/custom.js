66e5ba21069beaf06b4d81f1e80649bd7410e7a8
Frolov <vfrolov@vfrolov-mbp112.socal.rr.com>
2016-07-15 14:15:34 -0700

    code cleanup for formatting and spacing

--- js/custom.js
+++ js/custom.js
@@ -5,27 +5,24 @@ $(document).ready(function(){
     var $sli = $('#slider');
     var horizontalSections = $('div.horizontal-section').length;
     var verticalSections = $('div.vertical-section').length;
-    var WIDTH = $gal.width(); // 500
+    var WIDTH = $gal.width();
     var HEIGHT = $gal.height();
-    var horizontalCounter = 0;            // a counter
+    var horizontalCounter = 0;
     var verticalCounter = 0;
 
     $sli.width(WIDTH * horizontalSections);
     $sli.height(HEIGHT * verticalSections);
 
-
     $('#prev, #next').click(function() {
-        console.log(horizontalCounter)
         horizontalCounter = (this.id === 'next' ? ++horizontalCounter : --horizontalCounter) < 0 ? 
             horizontalSections - 1 : horizontalCounter%horizontalSections;
-        $sli.stop().animate({left: -horizontalCounter * WIDTH}, 400);
+        $sli.stop().animate({left: - (horizontalCounter * WIDTH) }, 400);
     });  
 
     $('#up, #down').click(function() {
-        console.log(verticalCounter)
         verticalCounter = (this.id === 'down' ? ++verticalCounter : --verticalCounter) < 0 ? 
-            verticalSections - 1 : verticalCounter%verticalSections;
-        $sli.stop().animate({top: - verticalCounter * HEIGHT}, 400);
+            verticalSections - 1 : verticalCounter % verticalSections;
+        $sli.stop().animate({top: - (verticalCounter * HEIGHT) }, 400);
     });
 
 });
