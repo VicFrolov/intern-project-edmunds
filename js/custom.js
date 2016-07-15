7cf85a4f5589f4931127b09ef8dc110cc08da592
Frolov <vfrolov@vfrolov-mbp112.socal.rr.com>
2016-07-15 13:46:07 -0700

    slider working both horizontally and vertically now

--- js/custom.js
+++ js/custom.js
@@ -6,12 +6,12 @@ $(document).ready(function(){
     var horizontalSections = $('div.horizontal-section').length;
     var verticalSections = $('div.vertical-section').length;
     var WIDTH = $gal.width(); // 500
+    var HEIGHT = $gal.height();
     var horizontalCounter = 0;            // a counter
     var verticalCounter = 0;
 
     $sli.width(WIDTH * horizontalSections);
-
-    console.log(horizontalSections)
+    $sli.height(HEIGHT * verticalSections);
 
 
     $('#prev, #next').click(function() {
@@ -20,5 +20,12 @@ $(document).ready(function(){
             horizontalSections - 1 : horizontalCounter%horizontalSections;
         $sli.stop().animate({left: -horizontalCounter * WIDTH}, 400);
     });  
+
+    $('#up, #down').click(function() {
+        console.log(verticalCounter)
+        verticalCounter = (this.id === 'down' ? ++verticalCounter : --verticalCounter) < 0 ? 
+            verticalSections - 1 : verticalCounter%verticalSections;
+        $sli.stop().animate({top: - verticalCounter * HEIGHT}, 400);
+    });
 
 });
