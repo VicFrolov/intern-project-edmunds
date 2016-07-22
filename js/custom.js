c4a91e058e11996bb4d6ee8e43582f71789bfd9c
Victor Frolov <vicfrolov@gmail.com>
2016-07-21 23:13:54 -0700

    removed old carrouseel slider js, and fixed missing divs for right hnaded div to display in research page

--- js/custom.js
+++ js/custom.js
@@ -2,46 +2,6 @@ $(document).ready(function(){
     $('body').removeClass('fade-out');
     $('.slider').slider({full_width: true});
 
-    var $gal = $('#gallery');
-    var $sli = $('#slider');
-    var horizontalSections = $('div.horizontal-section').length;
-    var verticalSections = $('div.vertical-section').length;
-    var WIDTH = $gal.width();
-    var HEIGHT = $gal.height();
-    var horizontalCounter = 0;
-    var verticalCounter = 0;
-
-    $sli.width(WIDTH * horizontalSections);
-    $sli.height(HEIGHT * verticalSections);
-
-    $('#prev, #next, #personalized-button').click(function() {
-        if (this.id === 'personalized-button' || this.id === 'next') {
-            horizontalCounter += 1;
-        } else {
-            horizontalCounter -= 1;
-        }
-
-        if (horizontalCounter >= horizontalSections) {
-            horizontalCounter = 0;
-        }
-
-        $sli.stop().animate({left: - (horizontalCounter * WIDTH) }, 400);
-    });  
-
-    $('#up, #down, #mmy-button').click(function() {
-        if (this.id === 'mmy-button' || this.id === "down") {
-            verticalCounter += 1;
-        } else {
-            verticalCounter -= 1;
-        }
-
-        if (verticalCounter >= verticalSections) {
-            verticalCounter = 0;
-        }
-
-        $sli.stop().animate({top: - (verticalCounter * HEIGHT) }, 400);
-    });
-
     /*Steve*/
     $(".steve_makes").click(function() {
         $(this).attr('class', 'steve_makes_selected');
@@ -52,5 +12,19 @@ $(document).ready(function(){
 
     $(".research-categories").click( function() {
         $(this).toggleClass("clicked");
-    })
+    });
+
+    var slider = document.getElementById('test5');
+    noUiSlider.create(slider, {
+        start: [20, 80],
+        connect: true,
+        step: 1,
+        range: {
+            'min': 0,
+            'max': 100
+        },
+            format: wNumb({
+            decimals: 0
+        })
+    });
 });
