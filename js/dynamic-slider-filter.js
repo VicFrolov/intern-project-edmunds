e028b3a48cd55e55b8b0766f4f60fdefb6c350a8
Victor Frolov <vicfrolov@gmail.com>
2016-08-03 01:14:32 -0700

    changed the add vehicle function to the new card panel laout

--- js/dynamic-slider-filter.js
+++ js/dynamic-slider-filter.js
@@ -19,9 +19,9 @@ $(document).ready(function () {
         }
     });
 
-    $("#car-panel").on("mouseenter", ".car-row", function (event) {
+    $("#car-panel").on("mouseenter", ".car-panel-card", function (event) {
         $(this).addClass('z-depth-1');
-    }).on("mouseleave", ".car-row", function (event) {
+    }).on("mouseleave", ".car-panel-card", function (event) {
         $(this).removeClass('z-depth-1');
     });
 
@@ -43,29 +43,35 @@ $(document).ready(function () {
     var fade_delay = 0
     // add individual car, with optional flags to display
     var add_car_to_panel = function (car_array) {
-        var row_to_add = "<tr>"; 
+        var card_panel = "";
 
-        for (var i = 0; i < car_array.length; i += 1) {
-            if (i === 0) {
-                row_to_add += "<td><img class='responsive-img' src='../img/car-panel/" + car_array[0] + "'></td>";
-            } else {
-                row_to_add += "<td"
-                if (car_array[i] === "A" || car_array[i] === "A-") {
-                    row_to_add += " class='green-review'";
-                } else if (car_array[i] === "B" || car_array[i] === "B-") {
-                    row_to_add += " class='orange-review'";
-                } else if (car_array[i] === "C" || car_array[i] === "C-") {
-                    row_to_add += " class='red-review'";
-                }
+        card_panel = "";
+        card_panel += "<div class='car-panel-card'>";
+        
+        card_panel += "<div class='car-panel-left'>";
+        card_panel += "<img class='car-thumbnail' src='../img/car-panel/" + car_array[0] + "'/>";
+        card_panel += "<div class='match-suffix'>match</div>";
+        card_panel += "<div class='match-percent'>" + car_array[10] + "%</div></div>";
+        
+        card_panel += "<div class='car-panel-middle'>" + "<div class='car-make'>" + car_array[1] + "</div>";
+        card_panel += "<div class='car-price'>" + car_array[2] + "</div>";
+        card_panel += "<div class='car-info'>" + "<div class='panel-selection'>" + "mileage: " + 
+            "<span class='panel-inject-1'>45</span>" + "</div>";
+        card_panel += "<div class='panel-selection'> safety: <span class='panel-inject-2'>A</span> + </div>";
+        card_panel += "<div class='panel-selection'> comfort: <span class='panel-inject-3'>A-</span> </div>";
+        card_panel += "</div></div>";
 
-               row_to_add += ">" + car_array[i] + "</td>"
-            }
-        }
+        card_panel += "<div class='car-panel-right'>";
+        card_panel += "<p class='edmunds-rating-suffix'>Edmund's Rating:</p>";
+        card_panel += "<p class='edmunds-rating'>" +  car_array[4] + "</p>";
+        card_panel += "<div class='blurb-suffix'> Our take:" + "<div class='blurb'> " + 
+            "This car drives like it looks, Amazing. The interior has a brand new (...)";
+        card_panel += "</div></div></div></div>"
 
-        row_to_add += '</tr>'
+
 
         fade_delay += 100;
-        $(row_to_add).hide().delay(fade_delay).appendTo("#car-panel-list").fadeIn(3000);
+        $(card_panel).hide().delay(fade_delay).appendTo("#car-panel").fadeIn(3000);
     }
 
     //Call this function every time the array 'ranking' is changed. This will paint
@@ -92,17 +98,17 @@ $(document).ready(function () {
     });
 
     var sedan_car_list =  [
-        ["bmw-3.png", "BMW 328i", "$52,450", 1, "A", "A", "A-", "A-", "B", "A"],
-        ["bmw-5.png", "BMW 528i", "$54,950", 2, "A", "A", "A-", "A", "A", "A"],
-        ["bmw-7.png", "BMW 728i", "$57,350", 3, "A", "A", "B", "B-", "A", "B"],
-        ["bmw-i3.png", "BMW i3", "$32,000", 4, "A-", "C", "A", "A-", "B", "A"], 
-        ["buick-lacrosse.png", "Buick Lacrosse", "$28,999", 5, "B", "B-", "B", "A-", "B", "A"],
-        ["buick-regal.png", "Buick Regal", "$32,999", 6, "B", "C", "A-", "C-", "C", "B"],
-        ["cadillac-ats.png", "Cadillac ATS", "$29,950", 7, "A", "A", "C", "A-", "C", "A"],
-        ["cadillac-ct6.png", "Cadillac CT6", "$32,499", 8, "B", "C", "A-", "B-", "B", "B"],
-        ["chrysler-200.png", "Chrysler 200", "$28,450", 9, "A-", "B", "B-", "C", "C", "C" ],
-        ["chrysler-300.png", "Chrysler 300", "$38,999", 10, "A-", "B", "B-" , "C-", "C", "B"],
-        ["dodge-charger.png", "Dodge Charger", "$22,450", 11, "A-", "B", "B-", "B-", "B", "B-"]
+        ["bmw-3.png", "BMW 328i", "$52,450", 1, "A", "A", "A-", "A-", "B", "A", 50],
+        ["bmw-5.png", "BMW 528i", "$54,950", 2, "A", "A", "A-", "A", "A", "A", 50],
+        ["bmw-7.png", "BMW 728i", "$57,350", 3, "A", "A", "B", "B-", "A", "B", 50],
+        ["bmw-i3.png", "BMW i3", "$32,000", 4, "A-", "C", "A", "A-", "B", "A", 50], 
+        ["buick-lacrosse.png", "Buick Lacrosse", "$28,999", 5, "B", "B-", "B", "A-", "B", "A", 50],
+        ["buick-regal.png", "Buick Regal", "$32,999", 6, "B", "C", "A-", "C-", "C", "B", 50],
+        ["cadillac-ats.png", "Cadillac ATS", "$29,950", 7, "A", "A", "C", "A-", "C", "A", 50],
+        ["cadillac-ct6.png", "Cadillac CT6", "$32,499", 8, "B", "C", "A-", "B-", "B", "B", 50],
+        ["chrysler-200.png", "Chrysler 200", "$28,450", 9, "A-", "B", "B-", "C", "C", "C", 50],
+        ["chrysler-300.png", "Chrysler 300", "$38,999", 10, "A-", "B", "B-" , "C-", "C", "B", 50],
+        ["dodge-charger.png", "Dodge Charger", "$22,450", 11, "A-", "B", "B-", "B-", "B", "B-", 50]
     ];
 
     var suv_car_list = [
@@ -126,13 +132,19 @@ $(document).ready(function () {
             $("#car-panel").animate({
                 width: "98%"
             }, 350);
+            $(".car-panel-card").animate({
+                width: "35%"
+            }, 350);            
         } else {
             $("#gallery").animate({
-                width: "70%"
+                width: "67%"
             }, 350)
             $("#car-panel").animate({
-                width: "29%"
+                width: "32%"
             }, 350);
+            $(".car-panel-card").animate({
+                width: "100%"
+            }, 350);             
         }
     });
 
